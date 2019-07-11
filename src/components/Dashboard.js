12c0bff4fb3d391db7e2b3a94a8339e0bb34b86f
Michael Palhidai <MPalhidai@gmail.com>
2019-07-10 22:20:40 -0700

    dashboard ideas

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -5,8 +5,14 @@ const Dashboard = () => {
   return (
     <Jumbotron>
       <Container>
-        D3 graph with all account values
-        umbotron>
+        <ul>
+          <li>D3 graph with all loans and transactions</li>
+          <li>bar graph multiple transactions</li>
+          <li>needs user defined time scaled x-axis</li>
+          <li>loans lent negative bar</li>
+          <li>payments received positive bar</li>
+          <li>D3 line graph overlay with interest accrued</li>
+        </ul>
       </Container>
     </Jumbotron>
   )
